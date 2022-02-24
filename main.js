9e610f07dd5334318c7aa7bf1af8f42bd1e04d2d
viswaFED <100114308+viswaFED@users.noreply.github.com>
2022-02-24 11:50:54 +0530

    localstorage

--- main.js
+++ main.js
@@ -34,3 +34,26 @@ function saveToLocalStorage(event){
     localStorage.setItem('email', email);
 
 }
+let myObj ={
+    name:'domenic',
+    age : 56
+    };
+    let myObj_serialized = JSON.stringify(myObj);
+     
+    localStorage.setItem("myObj",myObj_serialized);
+    
+    let myObj_deserialized = JSON.parse(localStorage.getItem("myObj"));
+    
+    console.log(myObj_deserialized);
+    
+
+let person = {
+    name: "John",
+    age: 22
+    };
+    
+    localStorage.setItem("profile", person);
+    localStorage.setItem("profile", JSON.stringify(person));
+    let storageProfileString = localStorage.getItem("profile");
+    let savedPerson = JSON.parse(storageProfileString);
+    console.log( savedPerson);
